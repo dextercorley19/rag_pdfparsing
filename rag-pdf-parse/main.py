2ee9629b4e113bc200218e3a8514c2692838276d
Dexter Corley <dexter.corley@live.com>
2024-05-30 23:19:02 -0700

    Working on chunking, fixed pdfminer outputting the text as a generator object. NLTK seems good.

--- rag-pdf-parse/main.py
+++ rag-pdf-parse/main.py
@@ -1,4 +1,22 @@
-from pdfminer.high_level import extract_text
+from io import StringIO
+from pdfminer.high_level import extract_text_to_fp
+from pdfminer.layout import LAParams
+import nltk
 
-text = extract_text("/app/PenaltyBoxIII-OperatingManual.pdf")
-print(text)
+
+output_string = StringIO()
+with open('/app/PenaltyBoxIII-OperatingManual.pdf', 'rb') as fin:
+    extract_text_to_fp(fin, output_string, laparams=LAParams(),
+                       output_type='text', codec=None) #can be 'html', 'xml', 'text', 'tag'
+    
+# Get the extracted text
+extracted_text = output_string.getvalue()
+
+# Tokenize the text into sentences
+sentences = nltk.sent_tokenize(extracted_text)
+
+# Print sentences
+sentence_to_start_at = 40
+sentence_to_end_at = 50
+for sentence in sentences[sentence_to_start_at:sentence_to_end_at]:
+    print(sentence)
